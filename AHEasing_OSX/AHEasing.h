c0b0384ef1c8a1df2a0d782ab226ce822a6e80b8
Bart Whiteley <bwhiteley@gmail.com>
2018-01-18 19:15:41 -0700

    include public headers in umbrella header

--- AHEasing_OSX/AHEasing.h
+++ AHEasing_OSX/AHEasing.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char AHEasingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AHEasing/PublicHeader.h>
 
+#import "CAKeyframeAnimation+AHEasing.h"
+#import "easing.h"
 
